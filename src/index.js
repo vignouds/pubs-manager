3a9203729a41b8d0a25aa4ae620fbf9fed1ca16e
Sébastien Vignoud <sebastien.vignoud@gmail.com>
2018-06-06 17:21:09 +0200

    TP 4.3.2 preque fini

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import pubService from "./services/pubs.services"
 
 pubService.listerPubsOuverts();
+document.getElementById('monBouton').addEventListener("click", function() {
+  pubService.insererPub();
+});
--- src/services/pubs.services.js
+++ src/services/pubs.services.js
@@ -6,7 +6,8 @@ import {Pub} from "../classes/Pub";
 import {OpenHours} from "../classes/OpenHours";
 export default {
     listerPubs,
-    listerPubsOuverts
+    listerPubsOuverts,
+    insererPub
   }
 
 function convertIntoPub() {
@@ -43,9 +44,29 @@ function listerPubs() {
 }
 
 function listerPubsOuverts() {
-  convertIntoPub().forEach(function(pub){
-    if (pub.openDays.includes(moment().format("dddd"))) {
-      writePub(pub);
+  // localStorage.clear();
+  if (localStorage.length == 0) {
+    convertIntoPub().forEach(function(pub){
+      if (pub.openDays.includes(moment().format("dddd"))) {
+        writePub(pub);
+      }
+    })
+    localStorage.setItem("pubs", JSON.stringify(convertIntoPub()));
+  }
+  else {
+    for (var i = 0; i < localStorage.length; i++) {
+      let pubObjet = JSON.parse(localStorage.getItem(localStorage.key(i)));
+      writePub(pubObjet);
     }
-  })
+  }
+}
+
+function insererPub() {
+  var nom = document.getElementById('nom').value;
+  var prenomProp = document.getElementById('prenomProp').value;
+  var nomProp = document.getElementById('nomProp').value;
+  var mailProp = document.getElementById('mailProp').value;
+  var prop = new Owner(prenomProp, nomProp, mailProp);
+  var newPub = new Pub(nom, prop);
+  myStorage.setItem('newPub', JSON.stringify(newPub));
 }
